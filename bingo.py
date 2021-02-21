e3c6a4f0aeaea007683e2c26985e9ae4a811bc4b
DestinationD <tusjecht@gmail.com>
2021-02-22 00:18:57 +0800

    Created basic bingo sim functions.

--- bingo.py
+++ bingo.py
@@ -1,54 +1,51 @@
-from random import choice, getrandbits, randint
+"""
+TODO:
+    Implement Matplotlib on VSC so I can test in real time
+    Figure out how to display multiple histograms on the same axes
+    Create biased draw methods
+    And probably some more stuff I forgot how to do before.
+"""
 
-roll_record = []
+from random import choice, getrandbits, randint
+import matplotlib.pyplot as plt
 
 def draw(biased):
     # assumption: biased is boolean or 0 & 1
-    if biased:
+    if bloated:
         return # return random_draw()
-    elif not biased:
-        return randint(1,36)
+    elif random:
+        draw_number = randint(1,36)
+        roll_record.append(draw_number)
+        return draw_number
     else:
         print("You messed up somewhere!")
 
 
-def check_win(board):
-    # We know our win conditions, so check
-    # the board
-    lines = 0
-    # Check horizontal and vertical lines
-    for i in range(6):
-            
-        if(all(x in roll_record for x in board[:][i])):
-            lines += 1
+def play_bingo(type):
+    # For many rounds of bingo, lines are of no use to us.
+    # Only (36-N) unique draws and (N * 10) dupes
+    board = 0
+    dupe_count = 0
+    draw_record = []
+    # Or board, dupe_count, draw_record = 0, 0, []
+    while board < 36:
+        draws += 1
+        if draw(type) in draw_record:
+            dupe_count += 1
+        else:
+            board += 1
+        if board + math.floor(dupe_count/10) == 36:
+            print("Bingo fully cleared!")
+        return draws
         
-        if(all(x in roll_record for x in board[i][:])):
-            lines += 1
-    
-    # Check diagonals
-    diag = [board[i][i] for i in range(6)]
-    ndiag = [board[i][~i] for i in range(6)]
-    
-    if(all(x in roll_record for x in diag)):
-        lines += 1
-    if(all(x in roll_record for x in ndiag)):
-        lines += 1
-    
-    if (lines == 0):
-        print("No lines have been formed.")
-    elif (lines < 14):
-        print(f'{lines} lines have been formed!')
-    else:
-        print("The board has been cleared!")
-        
-def roll(runs):
-    """
-    Creates a list equal to length of the parameter, filled with randomly drawn numbers.
-    """
-    roll_length = runs
-    for x in range(roll_length):
-        roll_record.append(draw(0))
-    print(roll_record)
-    
+def bingo_sim(samples, type):
+    draw_count = []
+    for i in range(samples):
+        draw_count.append(play_bingo(type))
+    x = list(range(50,105))
+    plt.hist(draw_count, bins = x)
 
-roll(36)
+# Until I figure out how to do this bit...
+if __name__ == "__main__:
+    x = list(range(50,105))
+    bingo_sim(1000, random)
